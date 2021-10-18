73d6bbe1621ce9183b1969559455db1ae422e852
Hallo89 <hallo89@protonmail.com>
2021-10-18 16:19:15 +0200

    KeyControl: Implement gamepad support

--- script/keycontrol.js
+++ script/keycontrol.js
@@ -6,27 +6,75 @@ class KeyControl {
   };
   tags = {};
 
-  constructor(target) {
+  gamepadLoopID;
+  gamepads = {};
+
+  constructor(target, useGamepad) {
     target.addEventListener('keydown', this.keyEvent.bind(this));
     target.addEventListener('keyup', this.keyEvent.bind(this));
+
+    if (useGamepad) {
+      window.addEventListener('gamepadconnected', this.gamepadConnected.bind(this));
+      window.addEventListener('gamepaddisconnected', this.gamepadDisconnected.bind(this));
+    }
   }
 
+  // ---- Gamepad functions ----
+  gamepadConnected(e) {
+    this.gamepads[e.gamepad.index] = e.gamepad;
+    this.gamepadLoopID = requestAnimationFrame(this.gamepadLoop.bind(this));
+  }
+  gamepadDisconnected(e) {
+    cancelAnimationFrame(this.gamepadLoopID);
+    delete this.gamepads[e.gamepad.index];
+  }
+
+  gamepadLoop(e) {
+    for (const gamepadIndex in this.gamepads) {
+      const gamepad = this.gamepads[gamepadIndex];
+
+      for (const actionName in this.actions.keydown) {
+        const action = this.actions.keydown[actionName];
+
+        const actionButtons = action.keys.filter(function(val) {
+          return typeof val == 'number';
+        });
+        for (const button of actionButtons) {
+          if (gamepad.buttons[button].pressed && (action.gamepadTimeout == null || action.$isReady)) {
+            action.fn.call(e.currentTarget, e, actionName);
+
+            if (action.gamepadTimeout != null) {
+              action.$isReady = false;
+              setTimeout(function() {
+                action.$isReady = true;
+              }, action.gamepadTimeout);
+            }
+          }
+        }
+      }
+    }
+
+    requestAnimationFrame(this.gamepadLoop.bind(this));
+  }
+
+  // ---- key functions ----
   keyEvent(e) {
     const eventActions = this.actions[e.type];
-    for (var prop in eventActions) {
-      const action = eventActions[prop];
+    for (const actionName in eventActions) {
+      const action = eventActions[actionName];
       if (action.keys && (action.keys.includes(e.key) || action.keys.includes('{' + e.code + '}'))) {
-        action.fn.call(e.currentTarget, e, prop);
+        action.fn.call(e.currentTarget, e, actionName);
       }
     }
   }
 
+  // ---- Prototype functions ----
   addTag(tagName, config) {
     this.tags[tagName] = config;
   };
 
   registerAction(actionName, config) {
-    if (config.tags !== undefined) {
+    if ('tags' in config) {
       for (let i = 0; i < config.tags.length; i++) {
         const tagObj = this.tags[config.tags[i]];
         for (let tagProp in tagObj) {
@@ -36,6 +84,10 @@ class KeyControl {
         }
       }
       delete config.tags;
+    }
+
+    if ('gamepadTimeout' in config) {
+      config.$isReady = true;
     }
 
     const target = this.actions[!config.event || config.event !== 'keyup' ? 'keydown' : 'keyup'];
@@ -51,4 +103,3 @@ class KeyControl {
     delete this.actions[action];
   };
 }
-
